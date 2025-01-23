0750fc58e84ae7a82d9bbf9d22c644776f555cc2
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-01-23 15:53:04 +0900

    20250123

---
+++ test.js
@@ -0,0 +1,22 @@
+function over(obj){
+    obj.src="src/test2.jpg"
+  }
+
+function out(obj){
+    obj.src="src/test.jpg"
+    }
+
+document.write("<h3>AAAAAAAAA</h3>");
+console.log("Hello, World!");
+let ret=prompt("이름", "박")
+if(ret==null){
+    console.log("null case")
+}
+else if(ret==""){
+    alert("문자열 입력 없이 확인")
+}
+else{
+    confirm("확실합니까")
+}
+alert("문자열 입력 없이 확인")
+confirm("확실합니까")

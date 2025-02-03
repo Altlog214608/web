d931ff812b5bd70a571b3e16f39a95cbde27f25f
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-02-03 12:24:46 +0900

    20250203

--- test.js
+++ test.js
@@ -608,3 +608,34 @@ function bub() {
 //ctrlKey : 컨트롤 키 눌린 상태 여부
 //shiftKey : 시프트키 눌린 상태 여부
 //keyCode : 키코드 값 숫자
+
+let tds;
+let prevIndex = 0;
+let index = 0;
+window.onload = function () {
+  tds = document.getElementById("td");
+  tds[index].style.backgroundColor = "orchid";
+};
+window.onkeydown = function (e) {
+  switch (e.key) {
+    case "ArrowDown":
+      if (index / 3 >= 2) return;
+      index += 3;
+      break;
+    case "ArrowLeft":
+      if (index % 3 == 0) return;
+      index--;
+      break;
+    case "ArrowUp":
+      if (index / 3 < 1) return;
+      index -= 3;
+      break;
+    case "ArrowRight":
+      if (index % 3 == 2) return;
+      index++;
+      break;
+  }
+  tds[index].style.backgroundColor = "orchid";
+  tds[prevIndex].style.backgroundColor = "white";
+  prevIndex = index;
+};

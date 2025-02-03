39539a74060c02420a19672ccd79630ae1c14264
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-02-03 17:09:15 +0900

    20250203

--- imagegame.js
+++ imagegame.js
@@ -2,28 +2,26 @@ let tds;
 let prevIndex = 0;
 let index = 0;
 let count = 0;
-let temp;
+let temp = 0;
 
-let images = [
-  "https://i.namu.wiki/i/aS1idcmGRakrHL0fHH-Uh2DLD-TzOL7kRXa1VfxfkaIX87qtRSJFqwGN0wooR5URmHCMa3hnXxamG0V5C33RzA.png",
-  "https://i.namu.wiki/i/ufcOD4DL7JFw-NyyejOg4YHNc-5wrcnRyk91VIpLDFJmerUcAyry-jekGvtdm7AQK0suz9Q8Y6Po4N7g9oXp4w.png",
-  "https://i.namu.wiki/i/7auspAoZ28RABuxMnMRdgchFRMrUwlYYhT7qffS2IwD2XIxXokvA8dyW4yKfOm2ZoAj-TuAa5Tb1DNjrDs7nOQ.png",
-  "https://i.namu.wiki/i/VtWNesGB9ppd00F8pyUXjeRv07sxDoBu1n509dHOKmZVl3Hp5YaGTn_PCL7H6oiq4ycS0Xe4szWcR6v1b_DbuA.png",
-  "https://i.namu.wiki/i/E-cKUfJs_Xyre5742iuz1eSzywKkQL5hIvF-KjkJfEeO_WiALVjy8kN0tZiKqLonO-kUp1tZDUYBIR97L0ZGEg.png",
-  "https://i.namu.wiki/i/3tUnbMpd9tswUT6IAjDXgKOiEBnCeIGEzJC1h8Vr_Fq-nEV618nhQxCM_AJLu0oEACxJXC5Ve5SZTCrzmPkmtA.png",
-  "https://i.namu.wiki/i/HjLTFaD3vFu-2T5MTV5E_DP76ZeFrLN6wZngLMVMCMAzcYjWMLCcenEi6foc5mrZ2HOOdj5EIhav-FC8YnCnew.png",
-  "https://i.namu.wiki/i/rlGaNoTytNFS2ZNT0YeLFzgnO7zYkbxf02IZN5ypkRxbTJLhtHzkgs6TwoWF6AEygFvsWt5qN3K2jitAqD1ttA.png",
-];
+let images = {
+  0: "https://i.namu.wiki/i/aS1idcmGRakrHL0fHH-Uh2DLD-TzOL7kRXa1VfxfkaIX87qtRSJFqwGN0wooR5URmHCMa3hnXxamG0V5C33RzA.png",
+  1: "https://i.namu.wiki/i/ufcOD4DL7JFw-NyyejOg4YHNc-5wrcnRyk91VIpLDFJmerUcAyry-jekGvtdm7AQK0suz9Q8Y6Po4N7g9oXp4w.png",
+  2: "https://i.namu.wiki/i/7auspAoZ28RABuxMnMRdgchFRMrUwlYYhT7qffS2IwD2XIxXokvA8dyW4yKfOm2ZoAj-TuAa5Tb1DNjrDs7nOQ.png",
+  3: "https://i.namu.wiki/i/VtWNesGB9ppd00F8pyUXjeRv07sxDoBu1n509dHOKmZVl3Hp5YaGTn_PCL7H6oiq4ycS0Xe4szWcR6v1b_DbuA.png",
+  4: "https://i.namu.wiki/i/E-cKUfJs_Xyre5742iuz1eSzywKkQL5hIvF-KjkJfEeO_WiALVjy8kN0tZiKqLonO-kUp1tZDUYBIR97L0ZGEg.png",
+  5: "https://i.namu.wiki/i/3tUnbMpd9tswUT6IAjDXgKOiEBnCeIGEzJC1h8Vr_Fq-nEV618nhQxCM_AJLu0oEACxJXC5Ve5SZTCrzmPkmtA.png",
+  6: "https://i.namu.wiki/i/HjLTFaD3vFu-2T5MTV5E_DP76ZeFrLN6wZngLMVMCMAzcYjWMLCcenEi6foc5mrZ2HOOdj5EIhav-FC8YnCnew.png",
+  7: "https://i.namu.wiki/i/rlGaNoTytNFS2ZNT0YeLFzgnO7zYkbxf02IZN5ypkRxbTJLhtHzkgs6TwoWF6AEygFvsWt5qN3K2jitAqD1ttA.png",
+};
 
 var counter = document.getElementById("counter");
 window.addEventListener("load", function () {
   tds = document.getElementsByTagName("td");
   startImage();
-  temp = tds[0].style.backgroundImage = "url(" + images[0] + ")";
 });
 
 window.addEventListener("keydown", function (e) {
-  console.log(e.key);
   switch (e.key) {
     case "ArrowDown":
       if (index + 4 < tds.length) index += 4;
@@ -42,9 +40,16 @@ window.addEventListener("keydown", function (e) {
       changeColor();
       break;
     case "Enter":
+      console.log(temp);
       openimage();
       count++;
       counter.innerHTML = count;
+      imagecheck();
+      if (index != 0) {
+        temp = index;
+      }
+      console.log(index);
+
       break;
   }
 });
@@ -58,34 +63,39 @@ function changeColor() {
 }
 
 function imagecheck() {
-  if (tds[index].style.backgroundImage === temp) {
-    alert("같은 이미지");
-  } else {
-    setTimeout(() => {
-      tds[index].style.backgroundPosition = "110px";
-    }, 100);
-    temp = tds[index].style.backgroundImage;
+  if (temp != 0) {
+    if (tds[index].style.backgroundImage !== tds[temp].style.backgroundImage) {
+      setTimeout(() => {
+        for (let i = 0; i < tds.length; i++) {
+          tds[i].style.backgroundPosition = "150px";
+        }
+        temp = 0;
+      }, 1000);
+    } else {
+      temp = 0;
+    }
   }
 }
 function openimage() {
   tds[index].style.backgroundPosition = "center";
-  imagecheck();
 }
 
 function startImage() {
-  images.sort(() => Math.random() - 0.5);
+  let keys = Object.keys(images);
+  keys.sort(() => Math.random() - 0.5);
+  console.log(keys);
   for (let i = 0; i < tds.length / 2; i++) {
-    tds[i].style.backgroundImage = "url(" + images[i] + ")";
+    tds[i].style.backgroundImage = "url(" + images[keys[i]] + ")";
     tds[i].style.backgroundSize = "cover";
     tds[i].style.backgroundPosition = "110px";
     tds[i].style.backgroundRepeat = "no-repeat";
   }
-  images.sort(() => Math.random() - 0.5);
+  keys.sort(() => Math.random() - 0.5);
+  console.log(keys);
   for (let i = 8; i < tds.length; i++) {
-    tds[i].style.backgroundImage = "url(" + images[i - 8] + ")";
+    tds[i].style.backgroundImage = "url(" + images[keys[i - 8]] + ")";
     tds[i].style.backgroundSize = "cover";
     tds[i].style.backgroundPosition = "110px";
     tds[i].style.backgroundRepeat = "no-repeat";
   }
-  tds[0].style.backgroundPosition = "center";
 }

d36af877c286e208a3e004d7e9908575c6d49222
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-02-04 12:38:40 +0900

    20250204

--- test.js
+++ test.js
@@ -746,8 +746,12 @@ function closeNewWindow() {
 
 //setTimeout()을 통해서 5초간 div 박스에 마우스를 올려놓으면 새로운 윈도우가 하나 열리도록 해보세요
 var box = document.getElementById("windowbox");
+let a;
 
 box.addEventListener("mouseover", () => {
-  setTimeout('load("https://www.naver.com")', 3000);
-  clearTimeout();
+  a = setTimeout('load("https://www.naver.com")', 3000);
+});
+
+box.addEventListener("mouseout", () => {
+  clearTimeout(a);
 });

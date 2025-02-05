13e86e9510c7f259a1a649e32ade406a3833c7ba
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-02-05 16:21:05 +0900

    20250205

--- nnn.js
+++ nnn.js
@@ -1,23 +1,30 @@
 var img = document.getElementById("imgbox");
 let x;
 let y;
-
+let bool = false;
 img.style.background =
   "url(https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/f373094a5e1a48c4826431bfb8253593_9366/OG_Blue_ID1454_01_standard.jpg)";
 img.style.backgroundRepeat = "no-repeat";
-img.style.backgroundPosition = "50,50";
+img.style.backgroundPosition = "center";
 img.style.backgroundSize = "cover";
-img.style.backgroundColor = "skyblue";
-img.style.width = 500;
-img.style.height = 500;
 
 img.addEventListener("mousemove", (e) => {
-  console.log(e.offsetX, e.offsetY);
   x = e.offsetX;
   y = e.offsetY;
+  xx = 600 - x;
+  yy = 600 - y;
+  if (bool == true) {
+    img.style.backgroundPosition = xx + "px " + yy + "px";
+  }
+});
 
-  img.style.width = img.style.width + x;
+img.addEventListener("mouseover", () => {
+  bool = true;
+  img.style.size = "600px 600px";
+});
 
-  img.style.backgroundPosition = x;
-  img.style.backgroundPosition = y;
+img.addEventListener("mouseout", () => {
+  bool = false;
+  img.style.size = "300px 300px";
+  img.style.backgroundPosition = "center";
 });

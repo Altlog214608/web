71afe3c9939160e2d5bde0304cad9b3d8ee5621f
Altlog214608 <kimdonghyeon1126@gmail.com>
2025-02-05 16:40:23 +0900

    20250205

--- nnn.js
+++ nnn.js
@@ -2,15 +2,15 @@ var img = document.getElementById("imgbox");
 let x;
 let y;
 let bool = false;
-img.style.background =
+img.style.backgroundImage =
   "url(https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/f373094a5e1a48c4826431bfb8253593_9366/OG_Blue_ID1454_01_standard.jpg)";
-img.style.backgroundRepeat = "no-repeat";
 img.style.backgroundPosition = "center";
 img.style.backgroundSize = "cover";
 
 img.addEventListener("mousemove", (e) => {
   x = e.offsetX;
   y = e.offsetY;
+  console.log(x, y);
   xx = 600 - x;
   yy = 600 - y;
   if (bool == true) {
@@ -20,11 +20,11 @@ img.addEventListener("mousemove", (e) => {
 
 img.addEventListener("mouseover", () => {
   bool = true;
-  img.style.size = "600px 600px";
+  img.style.backgroundSize = "600px 600px";
 });
 
 img.addEventListener("mouseout", () => {
   bool = false;
-  img.style.size = "300px 300px";
+  img.style.backgroundSize = "300px 300px";
   img.style.backgroundPosition = "center";
 });
